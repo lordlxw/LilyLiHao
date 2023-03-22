d9bedabd4b01ebc6544413a7e086be60ad9e38ae
周佳洪 <zhoujiahong@qq.com>
2023-03-22 15:39:20 +0800

    缩放

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,10 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss-pxtorem": {
+      "rootValue": 192, // 设计稿宽度的1/10
+      "propList": ["*"] // 需要做转化处理的属性，如`hight`、`width`、`margin`等，`*`表示全部
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import { sync } from 'vuex-router-sync'
 import router from './router'
 import store from './store'
+import "amfe-flexible"
 import '@/assets/theme/index.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
