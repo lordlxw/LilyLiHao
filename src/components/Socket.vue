9e78c49c5f3e4ca1f8f2e755a135d20f32d05033
周佳洪 <zhoujiahong@qq.com>
2023-06-08 17:30:42 +0800

    撤单取消交易员消息发送

--- src/components/Socket.vue
+++ src/components/Socket.vue
@@ -363,6 +363,10 @@ export default {
               // 撤单确认
               case 'request_cancel_bond_0':
               case 'request_cancel_bond_1':
+                self.$store.commit('SET_ENQUIRY_INFO', new Date().getTime() + '-' + Math.random(100000))
+                if (msgJson.actionType === 'refresh') {
+                  break
+                }
                 notify = self.$notify({
                   title: `${msgJson.data.createuser} 发起撤单`,
                   dangerouslyUseHTMLString: true,
@@ -420,7 +424,6 @@ export default {
                   ),
                   duration: 0
                 });
-                self.$store.commit('SET_ENQUIRY_INFO', new Date().getTime() + '-' + Math.random(100000))
                 self.$refs.playAudio.play()
                 self.notifyRejection[msgJson.data.userTradeId] = notify
                 // if (self.dialogTableVisible) {
@@ -429,38 +432,6 @@ export default {
                 break
               case 'deny_deal_bond_0':
               case 'deny_deal_bond_1':
-                // self.$notify({
-                //   title: `${msgJson.data.yanjiuyuanName} 已拒绝成交`,
-                //   dangerouslyUseHTMLString: true,
-                //   position: 'bottom-left',
-                //   message: `
-                //   <div class="notify">
-                //     <dl>
-                //       <dt>债券码</dt>
-                //       <dd>${msgJson.data.tscode}</dd>
-                //     </dl>
-                //     <dl>
-                //       <dt>方向</dt>
-                //       <dd>${msgJson.data.direction === 'bond_0' ? '买入' : msgJson.data.direction === 'bond_1' ? '卖出' : ''}</dd>
-                //     </dl>
-                //     <dl>
-                //       <dt>成交价</dt>
-                //       <dd>${util.moneyFormat(msgJson.data.price, 4)}</dd>
-                //     </dl>
-                //     <dl>
-                //       <dt>成交量</dt>
-                //       <dd>${msgJson.data.volume}</dd>
-                //     </dl>
-                //     <dl>
-                //       <dt>交割日期</dt>
-                //       <dd>${msgJson.data.deliveryTime.substr(0, 10)}</dd>
-                //     </dl>
-                //   </div>
-                //   `,
-                //   duration: 0
-                // });
-                // self.$store.commit('SET_ENQUIRY_INFO', new Date().getTime() + '-' + Math.random(100000))
-                // self.tryPlay()
                 self.$store.commit('SET_ENQUIRY_INFO', new Date().getTime() + '-' + Math.random(100000))
                 if (msgJson.actionType === 'refresh') {
                   break
